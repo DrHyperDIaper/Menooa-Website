497aedcea5c569f21f4ff83f11deef16df4ea81e
DrHyperDIaper <105463909+DrHyperDIaper@users.noreply.github.com>
2025-03-08 15:54:53 -0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,22 @@
 export const siteConfig = {
-  title: "Eric Minassian",
+  title: "Menooa Avrand",
   description:
     "SDE Intern at Amazon. Previously at Walmart. Studying CS + Business Administration at the University of California, Irvine.",
-  author: "Eric Minassian",
-  url: "https://www.ericminassian.com",
+  author: "Menooa Avrand",
+  url: "https://www.menooaavrand.com",
   themeColor: "#111010",
 } as const;
 
 export const personalConfig = {
-  name: "eric",
+  name: "menooa",
   emoji: "👋",
   description: `
-  I currently work as a Software Development Engineer at [Amazon](https://amazon.com),
+  I currently work as a Lead Steering Engineer at [Amazon](https://calsol.berkeley.edu/),
   where I work on [Project Kuiper](https://www.aboutamazon.com/what-we-do/devices-services/project-kuiper).
   
-  Previously, I worked at [Walmart Global Tech](https://tech.walmart.com) as a software engineer II. 
-  
-  I'm currently studying Computer Science & Business Administration at [University of California, Irvine](https://uci.edu).`,
+  I'm currently studying Mechanical Engineering at [University of California, Berkeley](https://me.berkeley.edu/).`,
   externalLinks: [
-    { href: "https://linkedin.com/in/minassian-eric", text: "linkedin" },
-    { href: "/Eric_Minassian_resume.pdf", text: "resume" },
-    { href: "https://github.com/eric-minassian", text: "github" },
-    { href: "mailto:eric@ericminassian.com", text: "email" },
+    { href: "https://www.linkedin.com/in/menooaavrand/", text: "linkedin" },
+    { href: "mailto:menooaavrand@berkeley.edu", text: "email" },
   ],
 } as const;
